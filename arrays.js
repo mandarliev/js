9a44919d90b0939cedc5f89f13ca578e549f1dcf
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-28 19:02:27 +0200

    Initial

---
+++ arrays.js
@@ -0,0 +1,7 @@
+// Arrays in JS - Basic Functions
+// The Array object is used to store multiple values in a single variable:
+
+// Full list of functions here: https://www.w3schools.com/js/js_array_methods.asp
+
+const students = ["Dimitar", "Ivan", "Sarah", "Nikola", "Jesus"];
+
