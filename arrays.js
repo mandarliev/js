8eb195cbefaedd4f26e1802e75b1604fa19ea020
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-28 19:13:58 +0200

    unshift

--- arrays.js
+++ arrays.js
@@ -8,21 +8,26 @@ const students = ["Dimitar", "Ivan", "Sarah", "Nikola", "Jesus"];
 // Arrays 101 - Push & Pop
 // Push adds new element and returns length of the array
 // Pop removes the last element of the array and returns it
-students.push('Kiril');
+students.push("Kiril");
 console.log(students);
 
 students.pop();
 console.log(students);
 
 // Arrays 101 - slice (it's non-inclusive)
-console.log(students.slice(1,3));
+console.log(students.slice(1, 3));
 
 // Arrays 101 - splice(start, end, item1, item2, itemX)
 // WARNING: Splice overwrites the original array
-students.splice(0, 3, 'Bob', 'Andy')
+students.splice(0, 3, "Bob", "Andy");
 console.log(students);
 
 // Arrays 101 - shift - removes first item of array and returns it
 // WARNING: shift overwrites the original array
 const firstStudent = students.shift();
 console.log(firstStudent);
+
+// Arrays 101 - unshift - adds new elements to the beginning of an array
+// WARNING: unshift overwrites the original array
+students.unshift("Apple", "Banana", "Orange");
+console.log(students);
