6da9830823d5e2dde978cbf6d60c9ac380ca5f31
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-31 14:31:49 +0200

    Traditional vs Arrow Functions

---
+++ traditionalFuncVsArrowFunc.js
@@ -0,0 +1,11 @@
+// Traditional
+function showName(name) {
+    console.log("I am superman");
+}
+
+showName();
+
+// Arrow
+const showName2 = (name) => console.log(`I am ${name}`);
+
+showName2('Dimitar');
